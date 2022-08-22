663337e3d95edfa11c29464e2571f2bde6ef2809
Aminul <mdaminulislamsahin14@gmail.com>
2022-08-22 19:11:26 +0600

    max 5 player validation

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@ const playerArray = []
 // Player Display Function
 function display(players) {
     const playerlistElement = document.getElementById("add-player-list");
+    const playerList = playerlistElement.children.length;
     const li = document.createElement("li");
 
     for (let i = 0; i < players.length; i++) {
       const player = players[i];
-      li.innerHTML = `<span class="block mb-5">${player}</span>`;
-      playerlistElement.appendChild(li);
+      if(playerList == 5) {
+        alert("You can add maximum 5 players");
+        return
+      }
+      else{
+          li.innerHTML = `<span class="block mb-5">${player}</span>`;
+          playerlistElement.appendChild(li);  
+      } 
     }
 }
 // Select Button click funtion
